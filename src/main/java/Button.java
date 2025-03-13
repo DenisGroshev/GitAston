c6a6b5a06fbd40ae356e00bea2d8ebac9c87d4aa
skrillex007 <evil.dalas@yandex.ru>
2025-03-13 13:41:49 +0300

    addedButton

---
+++ src/main/java/Button.java
@@ -0,0 +1,5 @@
+public class Button {
+    public void button(){
+        System.out.println("Button is existgit");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $evil.dalas.yandex.ru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
